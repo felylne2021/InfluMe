d0f6fa7ac02d3a51a59a12c347360ef69d0f0a56
Febi Mettasari <49263044+felylne2021@users.noreply.github.com>
2022-06-01 21:05:14 +0700

    Edit Job Image done

--- InfluMe/Views/EditJobPage.xaml.cs
+++ InfluMe/Views/EditJobPage.xaml.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -34,6 +34,24 @@ namespace InfluMe.Views {
 
         private void SecDatePicker_OkButtonClicked(object sender, Syncfusion.XForms.Pickers.DateChangedEventArgs e) {
             secondPickerButton.Text = string.Format("{0:dd/MM/yyyy}", e.NewValue);
+        }
+
+        private async void Upload_Clicked(object sender, System.EventArgs e) {
+            FileResult res = await MediaPicker.PickPhotoAsync(new MediaPickerOptions {
+                Title = "Pick job thumbnail image"
+            });
+
+            if (res != null) {
+                Stream stream = await res.OpenReadAsync();
+                
+                imageView.Source = ImageSource.FromStream(() => stream);
+
+
+                byte[] imageArray = System.IO.File.ReadAllBytes($@"{res.FullPath}");
+                string base64ImageRepresentation = Convert.ToBase64String(imageArray);
+                imageBlob.Text = base64ImageRepresentation;
+            }
+
         }
     }
 }
